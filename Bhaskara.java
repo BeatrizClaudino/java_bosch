da7d99787e63c8781597acf3b12303be0b9a9a6e
Bia Claudino <105952054+BeatrizClaudino@users.noreply.github.com>
2022-09-14 11:17:54 -0300

    Add files via upload

---
+++ Bhaskara.java
@@ -0,0 +1,13 @@
+package bosch;
+
+public class Bhaskara {
+    public static void main(String[] args) {
+        double a = 1;
+        double b = 12;
+        double c = -13;
+        double delta;
+
+        delta = Math.pow(b,2)-4*a*c;
+        System.out.println("O delta é: " + delta);
+    }
+}
